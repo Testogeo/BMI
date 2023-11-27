bffa3b8b5d7eddc284dbdba31eb726acf11ccd8b
MihhFor~ <fr.mikle@ya.ru>
2023-11-27 19:09:15 +0300

    Fisrt modification

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {
     public float calculate(double weight, double height) {
         int index;
 
-        index = (int) (weight / Math.pow (height, 2));
+        index = (int) (weight / Math.pow(height, 2));
 
         return (int) index;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ import java.lang.module.FindException;
 
 public class Main {
     public static void main(String[] args) {
+
         BmiService service = new BmiService();
+
         double height = 1.87; // Рост в метрах
         double weight = 98;  // Вес в кг
-        int BMI = (int) service.calculate(weight, height); // Приводим к целому типу
 
-        System.out.println("BMI=" + BMI); // Печать результата в консоль
+        int bmi = (int) service.calculate(weight, height); // Приводим к целому типу
+
+        System.out.println("BMI=" + bmi); // Печать результата в консоль
     }
 }
